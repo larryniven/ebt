e0586d7faad5b6390bfd85909b16710ea3578167
Hao Tang <haotang@ttic.edu>
2015-04-24 17:48:06 -0500

    Explicitly write out hash function to 2- and 3-tuple

--- tuple.h
+++ tuple.h
@@ -36,6 +36,32 @@ namespace std {
         }
     };
 
+    template <class A, class B, class C>
+    struct hash<tuple<A, B, C>> {
+        size_t operator()(tuple<A, B, C> const& t) const noexcept
+        {
+            hash<A> a_hasher;
+            hash<B> b_hasher;
+            hash<C> c_hasher;
+
+            size_t result = a_hasher(std::get<0>(t));
+            ebt::hash_combine(result, b_hasher(std::get<1>(t)));
+            return ebt::hash_combine(result, c_hasher(std::get<2>(t)));
+        }
+    };
+
+    template <class A, class B>
+    struct hash<tuple<A, B>> {
+        size_t operator()(tuple<A, B> const& t) const noexcept
+        {
+            hash<A> a_hasher;
+            hash<B> b_hasher;
+
+            size_t result = a_hasher(std::get<0>(t));
+            return ebt::hash_combine(result, b_hasher(std::get<1>(t)));
+        }
+    };
+
     template <class... Args>
     std::ostream& operator<<(std::ostream& os, std::tuple<Args...> const& t)
     {
