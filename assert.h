01b2d0a90bad85dabcad48efeb344e37829e1a04
Hao Tang <haotang@csail.mit.edu>
2017-11-29 17:17:28 -0500

    Use throw instead of exit

--- assert.h
+++ assert.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include <sstream>
+#include <exception>
 
 namespace ebt {
 
@@ -12,9 +14,10 @@ namespace ebt {
     assert_equals(T const &expected, U const &actual)
     {
         if (expected != actual) {
-            std::cerr << "expected: <" << expected << "> but was: <"
+            std::ostringstream oss;
+            oss << "expected: <" << expected << "> but was: <"
                 << actual << ">" << std::endl;
-            exit(1);
+            throw std::logic_error(oss.str());
         }
     }
     
@@ -23,9 +26,10 @@ namespace ebt {
     assert_equals(T expected, U actual)
     {
         if (std::fabs(expected - actual) > std::fabs(expected) * 1e-6) {
-            std::cerr << "expected: <" << expected << "> but was: <"
+            std::ostringstream oss;
+            oss << "expected: <" << expected << "> but was: <"
                 << actual << ">" << std::endl;
-            exit(1);
+            throw std::logic_error(oss.str());
         }
     }
 
@@ -33,9 +37,10 @@ namespace ebt {
     void assert_equals(T expected, U actual, double eps)
     {
         if (std::fabs(expected - actual) > eps) {
-            std::cerr << "expected: <" << expected << "> but was: <"
+            std::ostringstream oss;
+            oss << "expected: <" << expected << "> but was: <"
                 << actual << ">" << std::endl;
-            exit(1);
+            throw std::logic_error(oss.str());
         }
     }
 
